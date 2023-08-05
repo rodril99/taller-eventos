7f9ec8fe955e6115aeb4c7b45464625cc59e5a12
Rodrigo Ramos <rodryger1@gmail.com>
2023-08-05 20:45:59 -0300

    evento

---
+++ eventos.js
@@ -0,0 +1,7 @@
+let boton = document.getElementById("saludarBtn");
+
+document.addEventListener("DOMContentLoaded", function() {  
+    boton.addEventListener("click", function() {
+      alert("Hola! Soy el div");
+    });
+  });
